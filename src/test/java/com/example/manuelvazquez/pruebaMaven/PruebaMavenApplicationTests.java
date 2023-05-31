86c3b6511687002cbc82e23124ca17195627b528
mvazgon <absimiliard@hotmail.com>
2023-05-31 09:51:18 +0200

    Add files via upload

---
+++ src/test/java/com/example/manuelvazquez/pruebaMaven/PruebaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manuelvazquez.pruebaMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
